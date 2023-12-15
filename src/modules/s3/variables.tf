cbd26e8eafd8ebd5ea67195be63b0203ae2477ec
andres0010 <jonnyandres0010@hotmail.com>
2023-12-14 22:35:28 -0500

    Update variables.tf

--- src/modules/s3/variables.tf
+++ src/modules/s3/variables.tf
@@ -9,3 +9,15 @@ variable "bucket_name" {
     type = string
     default = "cp-web-hosting-bucket-prueba"
 }
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "Public Subnet CIDR values"
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "azs" {
+  type        = list(string)
+  description = "Availability Zones"
+  default     = ["us-west-1a", "us-west-1b"]
+}
