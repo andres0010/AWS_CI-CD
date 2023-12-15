3c94aaf4d3ab1bd85467876834bfc4e83bf70395
andres0010 <jonnyandres0010@hotmail.com>
2023-12-14 21:46:01 -0500

    Update variables.tf

--- src/modules/s3/variables.tf
+++ src/modules/s3/variables.tf
@@ -1,9 +1,11 @@
 variable "aws_region" {
     description = "AWS Region"
     type = string
+    default = "us-east-1"
 }
 
 variable "bucket_name" {
     description = "Name of the bucket"
     type = string
+    default = "cp-web-hosting-bucket"
 }
